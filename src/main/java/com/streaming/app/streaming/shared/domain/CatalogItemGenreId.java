356bb49d284b2f8e09abf507306e1ebb77a48aef
JuanC <abjuancarlos.12@gmail.com>
2024-09-25 01:58:12 -0500

    feat: VO created for id of CatalogItemGenre

---
+++ src/main/java/com/streaming/app/streaming/shared/domain/CatalogItemGenreId.java
@@ -0,0 +1,13 @@
+package com.streaming.app.streaming.shared.domain;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class CatalogItemGenreId {
+
+    private int value;
+
+    public int value() {
+        return value;
+    }
+}
