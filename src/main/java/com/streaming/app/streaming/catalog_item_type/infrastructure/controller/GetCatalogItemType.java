91b856724fc922925807d5d7823811ee52b1d929
JuanC <abjuancarlos.12@gmail.com>
2024-09-25 03:29:12 -0500

    feat: Controller to find all types was created

--- src/main/java/com/streaming/app/streaming/catalog_item_type/infrastructure/controller/GetCatalogItemType.java
+++ src/main/java/com/streaming/app/streaming/catalog_item_type/infrastructure/controller/GetCatalogItemType.java
@@ -1,6 +1,6 @@
 package com.streaming.app.streaming.catalog_item_type.infrastructure.controller;
 
-import com.streaming.app.streaming.catalog_item_type.service.find.CatalogItemGenreFinder;
+import com.streaming.app.streaming.catalog_item_type.service.find.CatalogItemTypeFinder;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-public class GetCatalogItemGenre {
+public class GetCatalogItemType {
 
-    private final CatalogItemGenreFinder service;
+    private final CatalogItemTypeFinder service;
 
-    @Operation(summary = "Get all genres", description = "Get all genres", tags = {"Genre"})
-    @GetMapping("/catalog_item_genre/")
+    @Operation(summary = "Get all types", description = "Get all types", tags = {"Types"})
+    @GetMapping("/v1/catalog-item/types/")
     public ResponseEntity<?> getCatalogItemGenre() {
         return ResponseEntity.ok(service.findAll());
     }
