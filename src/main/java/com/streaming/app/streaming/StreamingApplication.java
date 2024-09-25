ea2c4200fe141ed7a4b27c53658503738d265ffb
JuanC <abjuancarlos.12@gmail.com>
2024-09-25 01:30:59 -0500

    feat: create application

---
+++ src/main/java/com/streaming/app/streaming/StreamingApplication.java
@@ -0,0 +1,13 @@
+package com.streaming.app.streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingApplication.class, args);
+	}
+
+}
