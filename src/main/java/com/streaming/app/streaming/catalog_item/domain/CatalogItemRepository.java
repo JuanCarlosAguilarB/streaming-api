f31aa675968c16529c5a09150c301d1cfbf3fef6
JuanC <abjuancarlos.12@gmail.com>
2024-09-25 21:47:58 -0500

    feat: repository updated with necessary methods

--- src/main/java/com/streaming/app/streaming/catalog_item/domain/CatalogItemRepository.java
+++ src/main/java/com/streaming/app/streaming/catalog_item/domain/CatalogItemRepository.java
@@ -1,9 +1,16 @@
 package com.streaming.app.streaming.catalog_item.domain;
 
+import java.util.List;
+
 public interface CatalogItemRepository {
 
     public void save(CatalogItem catalogItem);
 
+    public int count();
+
     public CatalogItem findById(CatalogItemId id);
 
+    public List<CatalogItem> findByTitle(String title);
+    public List<CatalogItem> findByGenre(String genre);
+    public List<CatalogItem> findByOrderByViewsDesc();
 }
