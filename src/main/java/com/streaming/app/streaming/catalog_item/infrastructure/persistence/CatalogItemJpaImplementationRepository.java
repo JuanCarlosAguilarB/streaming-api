a8d090c466570e1b5459dca256d9e4d7dd96a6a9
JuanC <abjuancarlos.12@gmail.com>
2024-09-25 22:01:54 -0500

    feat: finished repository implementation.

--- src/main/java/com/streaming/app/streaming/catalog_item/infrastructure/persistence/CatalogItemJpaImplementationRepository.java
+++ src/main/java/com/streaming/app/streaming/catalog_item/infrastructure/persistence/CatalogItemJpaImplementationRepository.java
@@ -3,37 +3,49 @@ package com.streaming.app.streaming.catalog_item.infrastructure.persistence;
 import com.streaming.app.streaming.catalog_item.domain.CatalogItem;
 import com.streaming.app.streaming.catalog_item.domain.CatalogItemId;
 import com.streaming.app.streaming.catalog_item.domain.CatalogItemRepository;
+import com.streaming.app.streaming.catalog_item.domain.CatalogItemTitle;
+import com.streaming.app.streaming.shared.domain.CatalogItemGenreId;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import static com.streaming.app.streaming.catalog_item.infrastructure.persistence.CatalogItemMapper.toEntity;
+
+@Repository
+@AllArgsConstructor
 public class CatalogItemJpaImplementationRepository implements CatalogItemRepository {
+
+    private final CatalogItemJpaRepository repository;
+
     @Override
     public void save(CatalogItem catalogItem) {
-
+        CatalogItemEntity entity = toEntity(catalogItem);
+        repository.save(entity);
     }
 
     @Override
-    public int count() {
-        return 0;
+    public Long count() {
+        return repository.count();
     }
 
     @Override
     public CatalogItem findById(CatalogItemId id) {
-        return null;
+        return repository.findById(id.value()).map(CatalogItemMapper::toDomain).orElse(null);
     }
 
     @Override
-    public List<CatalogItem> findByTitle(String title) {
-        return List.of();
+    public List<CatalogItem> findByTitle(CatalogItemTitle title) {
+        return repository.findByTitle(title.value()).stream().map(CatalogItemMapper::toDomain).toList();
     }
 
     @Override
-    public List<CatalogItem> findByGenre(String genre) {
-        return List.of();
+    public List<CatalogItem> findByGenreId(CatalogItemGenreId genre) {
+        return repository.findByGenreId(genre.value()).stream().map(CatalogItemMapper::toDomain).toList();
     }
 
     @Override
     public List<CatalogItem> findByOrderByViewsDesc() {
-        return List.of();
+        return repository.findAllByOrderByViewsDesc().stream().map(CatalogItemMapper::toDomain).toList();
     }
 }
--- src/main/java/com/streaming/app/streaming/catalog_item/infrastructure/persistence/CatalogItemJpaRepository.java
+++ src/main/java/com/streaming/app/streaming/catalog_item/infrastructure/persistence/CatalogItemJpaRepository.java
@@ -2,7 +2,11 @@ package com.streaming.app.streaming.catalog_item.infrastructure.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CatalogItemJpaRepository extends JpaRepository<CatalogItemEntity, UUID> {
+    List<CatalogItemEntity> findByTitle(String value);
+    List<CatalogItemEntity> findByGenreId(int value);
+    List<CatalogItemEntity> findAllByOrderByViewsDesc();
 }
