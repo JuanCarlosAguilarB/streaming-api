7016b898c9993cc224c8887d71da6fc3a8e23b59
JuanC <abjuancarlos.12@gmail.com>
2024-09-25 21:51:21 -0500

    fix: fixed repository

--- src/main/java/com/streaming/app/streaming/catalog_item/infrastructure/persistence/CatalogItemJpaRepository.java
+++ src/main/java/com/streaming/app/streaming/catalog_item/infrastructure/persistence/CatalogItemJpaRepository.java
@@ -2,5 +2,7 @@ package com.streaming.app.streaming.catalog_item.infrastructure.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CatalogItemJpaRepository extends JpaRepository<CatalogItemEntity, Integer> {
+import java.util.UUID;
+
+public interface CatalogItemJpaRepository extends JpaRepository<CatalogItemEntity, UUID> {
 }
