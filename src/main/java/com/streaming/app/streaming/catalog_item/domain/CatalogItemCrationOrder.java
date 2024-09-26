e9333712729f08c6e0b8da3b1dc42f2ba439e3c9
JuanC <abjuancarlos.12@gmail.com>
2024-09-25 21:37:54 -0500

    fix: added attribute to the class

--- src/main/java/com/streaming/app/streaming/catalog_item/domain/CatalogItemCrationOrder.java
+++ src/main/java/com/streaming/app/streaming/catalog_item/domain/CatalogItemCrationOrder.java
@@ -1,4 +1,16 @@
 package com.streaming.app.streaming.catalog_item.domain;
 
+
 public class CatalogItemCrationOrder {
+
+    private int value;
+
+    public CatalogItemCrationOrder(int value) {
+        this.value = value;
+    }
+
+    public int value() {
+        return value;
+    }
+
 }
