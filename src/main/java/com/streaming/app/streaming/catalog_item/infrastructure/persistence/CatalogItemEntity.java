2babd0ca7ffe78ab922d198d290aad1877c8bb70
JuanC <abjuancarlos.12@gmail.com>
2024-09-25 21:29:48 -0500

    feat: CatalogItemEntity was created

---
+++ src/main/java/com/streaming/app/streaming/catalog_item/infrastructure/persistence/CatalogItemEntity.java
@@ -0,0 +1,42 @@
+package com.streaming.app.streaming.catalog_item.infrastructure.persistence;
+
+import com.streaming.app.streaming.catalog_item_genre.infrastructure.persistence.CatalogItemGenreEntity;
+import com.streaming.app.streaming.catalog_item_type.infrastructure.persistence.CatalogItemTypeEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+public class CatalogItemEntity {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String title;
+    private Integer views;
+    private Double averageScore;
+    private String description;
+    private String imageUrl;
+
+    private Integer creationOrder;
+
+    @ManyToOne(targetEntity = CatalogItemTypeEntity.class)
+    @JoinColumn(name = "type_id", referencedColumnName = "id", nullable = false)
+    private Integer typeId;
+
+
+    @ManyToOne(targetEntity = CatalogItemGenreEntity.class)
+    @JoinColumn(name = "genre_id", referencedColumnName = "id", nullable = false)
+    private Integer genreId;
+
+}
