f84f13917daef04df2bf48eb5572687d680fd6c1
JuanC <abjuancarlos.12@gmail.com>
2024-09-26 03:22:23 -0500

    feat: added method to get the user by email

--- src/main/java/com/streaming/app/streaming/auth/application/find/UserFinder.java
+++ src/main/java/com/streaming/app/streaming/auth/application/find/UserFinder.java
@@ -1,10 +1,7 @@
 package com.streaming.app.streaming.auth.application.find;
 
 
-import com.streaming.app.streaming.auth.domain.User;
-import com.streaming.app.streaming.auth.domain.UserNotFoundException;
-import com.streaming.app.streaming.auth.domain.UserRepository;
-import com.streaming.app.streaming.auth.domain.UserUserId;
+import com.streaming.app.streaming.auth.domain.*;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +14,11 @@ public class UserFinder {
     public User findById(UserUserId userId) {
         return repository.findById(userId)
                 .orElseThrow(() -> new UserNotFoundException("user not found with id: " + userId.value()));
+    }
+
+    public User findByEmail(UserEmail email) {
+        return repository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException("user not found with email: " + email.value()));
     }
 
 }
